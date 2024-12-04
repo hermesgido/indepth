056acb7be9a700b04807dda28349a5c9f69aea53
hermessgido <hermessgido@gmail.com>
2024-12-05 01:10:25 +0300

    path('customer/create

--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     # path('api/', views.home, name="home"),
     path('hello/', HelloWorldView.as_view(), name='hello-world'),
     path('customer/<str:phone_number>/', CustomerAPIView.as_view(), name='customer-detail'),
-    path('customer/create/', CustomerCreateAPIView.as_view(), name='customer-create'),
-    path('transaction/create/', TransactionCreateAPIView.as_view(), name='transaction-create'),
+    path('customer-create/', CustomerCreateAPIView.as_view(), name='customer-create'),
+    path('transaction/create/', TransactionCreateAPIView.as_view(),
+         name='transaction-create'),
     path('vending/', VendingMashineCallBackAPI.as_view(), name='vending-machine-callback'),
     path('machine-slots/<str:machine_id>', MachineSlotsAPIView.as_view(), name='machine-slots')
 ]
