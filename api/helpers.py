d3d76c5cdad98c31d73fd789e0ab09657f00df16
hermessgido <hermessgido@gmail.com>
2024-12-12 13:39:00 +0300

    slot number

--- api/helpers.py
+++ api/helpers.py
@@ -42,20 +42,20 @@ def polling_interface_4000(request):
         print("Peninggg obtained machine 22")
         pendings.status = "Processing"
         pendings.save()
+        # return Response({
+        #     "Status": "0",
+        #     "MsgType": "0",
+        #     "SlotNo": "13",
+        #     "ProductID": "222",  
+        #     "TradeNo": "3232323", 
+        #     "Err": "success"
+        # })
         return Response({
             "Status": "0",
             "MsgType": "0",
-            "SlotNo": "13",
-            "ProductID": "222",  # sample "222"
-            "TradeNo": "3232323",  # sample "3534647568"
-            "Err": "success"
-        })
-        return Response({
-            "Status": "0",
-            "MsgType": "0",
-            "SlotNo": pendings.slot.slot_number,
+            "SlotNo": str(pendings.slot.slot_number),
             "ProductID": "222", #sample "222"
-            "TradeNo": pendings.id, ##sample "3534647568"
+            "TradeNo": str(pendings.id),  # sample "3534647568"
             "Err": "success"
         })
     return Response({"Status": "1", "Err": "Invalid MsgType"}, status=400)
